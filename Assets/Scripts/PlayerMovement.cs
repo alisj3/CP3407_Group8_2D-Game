b4f031f1ef7dff54e168c42c974165e8d251d9fe
alisj3 <alisher123841@bk.ru>
2024-07-03 18:09:06 +0800

    Minor Changes

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,25 +4,24 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
     private float horizontal;
     private float speed = 8f;
     private float jumpingPower = 800f;
     private bool isFacingRight = true;
+    private bool canJump = true;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform GroundCheck;
     [SerializeField] private LayerMask groundLayer;
-
 
     // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && canJump)
         {
-            Debug.Log("wtf");
+            StartCoroutine(JumpCooldown());
             rb.AddForce(new Vector2(rb.velocity.x, jumpingPower));
         }
 
@@ -34,15 +33,21 @@ public class PlayerMovement : MonoBehaviour
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
     }
 
-
     private void Flip()
     {
-        if(isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
         {
             isFacingRight = !isFacingRight;
             Vector3 localScale = transform.localScale;
             localScale.x *= -1f;
             transform.localScale = localScale;
         }
+    }
+
+    private IEnumerator JumpCooldown()
+    {
+        canJump = false;
+        yield return new WaitForSeconds(0.8f);
+        canJump = true;
     }
 }
